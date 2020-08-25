360b096db78986b1b4e36a676e9ae53d23cd3ccd
Gabriel <knuckles4x@gmail.com>
2020-08-25 00:08:16 -0300

    ad banner + margin top

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -6,12 +6,17 @@ import DesktopHeader from "../DesktopHeader";
 import LeftColumn from "../LeftColumn";
 import MiddleColumn from "../MiddleColumn";
 import RightColumn from "../RightColumn";
+import AdBanner from "../AdBanner";
 
 const Layout: React.FC = () => {
   return (
     <Container>
       <MobileHeader />
       <DesktopHeader />
+
+      <span>
+        <AdBanner />
+      </span>
       <main>
         <LeftColumn />
         <MiddleColumn />
