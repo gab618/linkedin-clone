08fff77ddd21248b20ba793a26ca937be0c81987
Gabriel <knuckles4x@gmail.com>
2020-08-24 23:18:52 -0300

    mobile header

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { Container } from "./styles";
+import MobileHeader from "../MobileHeader";
 import LeftColumn from "../LeftColumn";
 import MiddleColumn from "../MiddleColumn";
 import RightColumn from "../RightColumn";
@@ -8,6 +9,7 @@ import RightColumn from "../RightColumn";
 const Layout: React.FC = () => {
   return (
     <Container>
+      <MobileHeader />
       <main>
         <LeftColumn />
         <MiddleColumn />
